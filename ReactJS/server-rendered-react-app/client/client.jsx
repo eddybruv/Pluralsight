43f54154183e7ba54964c0a7fa5ba2ab7ec69fbb
eddybruv <edwinajong@gmail.com>
2022-05-03 03:58:08 +0100

    added fetch apis

--- ReactJS/server-rendered-react-app/client/client.jsx
+++ ReactJS/server-rendered-react-app/client/client.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { handleModify } from "../shared/utility";
 
+let state = undefined;
 
-function render () {
-  ReactDOM.hydrate(<App/>, document.querySelector('#root'))
+fetch("http://localhost:7777/data")
+  .then((data) => data.json())
+  .then((json) => {
+    state = json;
+    console.log("Got the state", state);
+    render();
+  });
+
+function handleVote(answerId, increment) {
+  state.answers = handleModify(state.answers, answerId, increment);
+
+  fetch(`vote/${answerId}?increment=${increment}`);
+
+  render();
 }
 
-// ReactDOM.render(<App/>, document.getElementById("root"));
-render();
+function render() {
+  ReactDOM.hydrate(
+    <App {...state} handleModify={handleModify} />,
+    document.querySelector("#root")
+  );
+}
