6290910c8a91e25a5b1e4d0d7a8c50dcf48fc3e0
eddybruv <edwinajong@gmail.com>
2022-05-03 03:57:16 +0100

    handle function

---
+++ ReactJS/server-rendered-react-app/shared/utility.js
@@ -0,0 +1,10 @@
+export function handleModify(answers, answerId, increment) {
+  return answers.map((answer) => {
+    if (answer.answerId !== answerId) {
+      return answer;
+    } else {
+      return { ...answer, upvotes: answer.upvotes + increment };
+    }
+  });
+
+}
